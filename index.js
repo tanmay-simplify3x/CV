2d540dd436f7f9c53e38aba63f4ead509c1d0b56
Tanmay Singh <106429652+SinghTanmay024@users.noreply.github.com>
2022-12-18 20:28:39 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+$("#Like").click(function(){
+  $("h1").slideUp().slideDown().animate({opacity: 0.5});
+  $(".Like").css("color","red");
+  $("h1").css("color","red")
+});
+// $('#img').css('transform','rotate(90deg)');
